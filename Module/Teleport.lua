886abda7e9ba9eec32af38a1ab4add6e53fa948e
RedGamer12 <73015566+RedGamer12@users.noreply.github.com>
2023-06-30 13:59:34 +0700

    Update Teleport.lua

--- Module/Teleport.lua
+++ Module/Teleport.lua
@@ -4,7 +4,7 @@ local PlayerChecker = DependencyLoader.FastLoadDependencies("https://raw.githubu
 
 local TeleportModule = {}
 
-function TeleportModule.TweenService(player, pos, speed, found, toggle)
+function TeleportModule.TweenService1(player, pos, speed, found, toggle)
     if PlayerChecker.IsPlayerDead(player) then
         player.Character.HumanoidRootPart.CFrame = pos
         return
@@ -33,6 +33,47 @@ function TeleportModule.TweenService(player, pos, speed, found, toggle)
         player.Character.HumanoidRootPart.CFrame = val.Value
         task.wait()
     end
+
+    val:Destroy()
+end
+
+function TeleportModule.TweenService2(player, object, speed, found, toggle)
+    if PlayerChecker.IsPlayerDead(player) then
+        player.Character.HumanoidRootPart.CFrame = object.CFrame
+        return
+    end
+
+    local val = Instance.new("CFrameValue")
+    val.Value = player.Character.HumanoidRootPart.CFrame
+
+    local tween = game:GetService("TweenService"):Create(val,
+        TweenInfo.new((player.Character.HumanoidRootPart.Position - object.Position).magnitude / speed, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0),
+        {Value = object.CFrame}
+    )
+
+    tween:Play()
+
+    local function updateCFrame()
+        while true do
+            if found or not toggle or PlayerChecker.IsPlayerDead(player) then
+                tween:Cancel()
+                break
+            end
+            player.Character.HumanoidRootPart.CFrame = val.Value
+            task.wait()
+        end
+    end
+
+    spawn(updateCFrame)
+
+    local completed
+    tween.Completed:Connect(function()
+        completed = true
+    end)
+
+    repeat
+        wait()
+    until completed
 
     val:Destroy()
 end
